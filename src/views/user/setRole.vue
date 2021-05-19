4fa37677dc8da6f0cdcb3cf350e1307e47eb8a8c
wanglixiang <codegirl_wanglixiang@outlook.com>
2021-05-19 09:20:21 +0800

    增加角色权限设置的页面

---
+++ src/views/user/setRole.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="app-container">
+
+    <el-table
+      :key="tableKey"
+      v-loading="listLoading"
+      :data="list"
+      row-key="menuId"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;"
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      @sort-change="sortChange"
+    >
+      <el-table-column label="菜单Id" align="center" :class-name="getSortClass('id')">
+        <template slot-scope="{row}">
+          <span>{{ row.menuId }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="菜单名称" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.menuName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="跳转路径" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.route }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="页面功能">
+        <template slot-scope="{row}">
+          <el-tag v-for="(item,index) in JSON.parse(row.operation)" :key="index" size="mini" style="margin-left:10px;">{{ item.arryName }}</el-tag>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+
+  </div>
+</template>
+
+<script>
+import { getMenuList } from '@/api/MenuAPI'
+import waves from '@/directive/waves'
+import Pagination from '@/components/Pagination'
+
+export default {
+  name: 'ComplexTable',
+  components: { Pagination },
+  directives: { waves },
+  data() {
+    return {
+      tableKey: 0,
+      list: null,
+      total: 0,
+      listLoading: false,
+      listQuery: {
+        page: 1,
+        limit: 10,
+        sort: 'ASC',
+        sidx: 'sequence',
+        parameterJson: [
+        ]
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+  computed: {
+    optionData() {
+      return this.list
+    }
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      getMenuList(this.listQuery).then(response => {
+        this.list = response.data.item
+        this.total = response.data.total
+
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1.5 * 1000)
+      })
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    sortChange(data) {
+      const { prop, order } = data
+      if (prop === 'id') {
+        this.sortByID(order)
+      }
+    },
+    sortByID(order) {
+      if (order === 'ascending') {
+        this.listQuery.sort = '+id'
+      } else {
+        this.listQuery.sort = '-id'
+      }
+      this.handleFilter()
+    },
+    getSortClass: function(key) {
+      const sort = this.listQuery.sort
+      return sort === `+${key}` ? 'ascending' : 'descending'
+    }
+  }
+}
+</script>
